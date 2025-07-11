0347caa059806fe06210ce7989318a9737fc32cb
oishe1980 <70874342+oishe1980@users.noreply.github.com>
2025-07-11 14:16:15 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,11 @@
 const CACHE_NAME = 'iqmdb-cache-v1';
 const urlsToCache = [
   '/',
-  '/p/manifest.html',
+  '/?m=1'
 ];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
